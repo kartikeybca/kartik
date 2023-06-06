3f370afcc6af84ca7d535e40276504b695bc484c
kartikeybca <135728169+kartikeybca@users.noreply.github.com>
2023-06-06 21:18:59 +0530

    Add files via upload

---
+++ net.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace programm
+{
+    class Programm
+    {
+        static void Main(string[] args){
+            Stringbuilder sb = new Stringbuilder("this is the string builder sentence");
+            Console.WriteLine(sb);
+        }
+    }
+}
